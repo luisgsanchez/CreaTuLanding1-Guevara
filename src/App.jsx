45945f432dc50693ba6f874b8a0cfcad2b67e259
luisgsanchez <luiscarlosguevara469@gmail.com>
2025-12-22 21:13:11 -0500

    Create App component with NavBar and ItemListContainer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting="¡Bienvenido a mi tienda online!" />
+    </div>
+  );
+}
+
+export default App;
